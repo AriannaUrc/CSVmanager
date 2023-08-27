a4ccaf4482b0fff87ec4e59b6a7a03bd376f6e72
AriannaUrc <urciuoli.arianna.studente@itispaleocapa.it>
2023-08-27 17:26:53 +0200

    Add project files.

---
+++ CSVmanager/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace CSVmanager
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listView = new ListView();
+            columnHeader1 = new ColumnHeader();
+            columnHeader2 = new ColumnHeader();
+            columnHeader3 = new ColumnHeader();
+            columnHeader4 = new ColumnHeader();
+            OpenButton = new Button();
+            Add = new Button();
+            Modify_button = new Button();
+            modify_draw_date_textbox = new TextBox();
+            label1 = new Label();
+            groupBox1 = new GroupBox();
+            modify_new_value_textbox = new TextBox();
+            label3 = new Label();
+            modify_target = new ComboBox();
+            label2 = new Label();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // listView
+            // 
+            listView.BackColor = SystemColors.InactiveBorder;
+            listView.Columns.AddRange(new ColumnHeader[] { columnHeader1, columnHeader2, columnHeader3, columnHeader4 });
+            listView.GridLines = true;
+            listView.Location = new Point(434, 12);
+            listView.Name = "listView";
+            listView.Size = new Size(364, 387);
+            listView.TabIndex = 0;
+            listView.UseCompatibleStateImageBehavior = false;
+            listView.View = View.Details;
+            // 
+            // columnHeader1
+            // 
+            columnHeader1.Text = "DrawDate";
+            columnHeader1.Width = 90;
+            // 
+            // columnHeader2
+            // 
+            columnHeader2.Text = "WinNumbers";
+            columnHeader2.Width = 90;
+            // 
+            // columnHeader3
+            // 
+            columnHeader3.Text = "MegaBall";
+            columnHeader3.Width = 90;
+            // 
+            // columnHeader4
+            // 
+            columnHeader4.Text = "Multiplier";
+            columnHeader4.Width = 90;
+            // 
+            // OpenButton
+            // 
+            OpenButton.Location = new Point(713, 415);
+            OpenButton.Name = "OpenButton";
+            OpenButton.Size = new Size(75, 23);
+            OpenButton.TabIndex = 1;
+            OpenButton.Text = "Open";
+            OpenButton.UseVisualStyleBackColor = true;
+            OpenButton.Click += OpenButton_Click;
+            // 
+            // Add
+            // 
+            Add.Location = new Point(605, 415);
+            Add.Name = "Add";
+            Add.Size = new Size(75, 23);
+            Add.TabIndex = 2;
+            Add.Text = "Load";
+            Add.UseVisualStyleBackColor = true;
+            Add.Click += Add_Click;
+            // 
+            // Modify_button
+            // 
+            Modify_button.Location = new Point(6, 45);
+            Modify_button.Name = "Modify_button";
+            Modify_button.Size = new Size(75, 23);
+            Modify_button.TabIndex = 3;
+            Modify_button.Text = "Modify";
+            Modify_button.UseVisualStyleBackColor = true;
+            Modify_button.Click += Modify_button_Click;
+            // 
+            // modify_draw_date_textbox
+            // 
+            modify_draw_date_textbox.Location = new Point(96, 46);
+            modify_draw_date_textbox.Name = "modify_draw_date_textbox";
+            modify_draw_date_textbox.Size = new Size(90, 23);
+            modify_draw_date_textbox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(107, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(60, 15);
+            label1.TabIndex = 5;
+            label1.Text = "Draw date";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(modify_new_value_textbox);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(modify_target);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(modify_draw_date_textbox);
+            groupBox1.Controls.Add(Modify_button);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 5);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(406, 394);
+            groupBox1.TabIndex = 6;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "functions";
+            // 
+            // modify_new_value_textbox
+            // 
+            modify_new_value_textbox.Location = new Point(306, 46);
+            modify_new_value_textbox.Name = "modify_new_value_textbox";
+            modify_new_value_textbox.Size = new Size(90, 23);
+            modify_new_value_textbox.TabIndex = 8;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(317, 28);
+            label3.Name = "label3";
+            label3.Size = new Size(62, 15);
+            label3.TabIndex = 9;
+            label3.Text = "New Value";
+            // 
+            // modify_target
+            // 
+            modify_target.FormattingEnabled = true;
+            modify_target.Items.AddRange(new object[] { "WinNumbers", "MegaBall", "Multiplier" });
+            modify_target.Location = new Point(192, 45);
+            modify_target.Name = "modify_target";
+            modify_target.Size = new Size(106, 23);
+            modify_target.TabIndex = 7;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(226, 28);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 6;
+            label2.Text = "Target";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(810, 457);
+            Controls.Add(groupBox1);
+            Controls.Add(Add);
+            Controls.Add(OpenButton);
+            Controls.Add(listView);
+            FormBorderStyle = FormBorderStyle.FixedDialog;
+            Name = "Form1";
+            Text = "CSVmanager";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListView listView;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private Button OpenButton;
+        private Button Add;
+        private Button Modify_button;
+        private TextBox modify_draw_date_textbox;
+        private Label label1;
+        private GroupBox groupBox1;
+        private Label label2;
+        private ComboBox modify_target;
+        private TextBox modify_new_value_textbox;
+        private Label label3;
+    }
+}
---
+++ CSVmanager/Form1.cs
@@ -0,0 +1,264 @@
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace CSVmanager
+{
+    public partial class Form1 : Form
+    {
+        public struct dati
+        {
+            public string draw_date;
+            public string winning_numbers;
+            public int mega_ball;
+            public int multiplier;
+            public int miovalore;
+            public bool cancLogic;
+        }
+
+        public int recordLength;
+        public string separator;
+
+        public dati d;
+        public string FileName;
+        public string BackUp;
+
+        public Form1()
+        {
+            InitializeComponent();
+            recordLength = 64;
+            FileName = "Lottery_Numbers.csv";
+            BackUp = "BackUp.csv";
+            separator = ",";
+
+            Format();
+            LoadFromFile();
+        }
+
+
+        //dato un comando lo esegue appogiandosi allo shell
+        public void ExecuteCommand(string Command, string Arguments = "", string Path = "", bool ShellExecute = true)
+        {
+            ProcessStartInfo ProcessInfo = new ProcessStartInfo(Command);
+
+            if (Path != "")
+            {
+                ProcessInfo.WorkingDirectory = Path;
+            }
+            if (Arguments != "")
+            {
+                ProcessInfo.Arguments = Arguments;
+            }
+            ProcessInfo.CreateNoWindow = true;
+            ProcessInfo.UseShellExecute = ShellExecute;
+
+            //Process Process =
+            Process.Start(ProcessInfo);
+        }
+
+
+        //rende il file utilizzabile per l'accesso diretto
+        public void Format()
+        {
+            StreamReader sr = new StreamReader(BackUp);
+            StreamWriter sw = new StreamWriter(FileName, false);
+
+            string str = sr.ReadLine();
+            Random random = new Random();
+
+            while (str != null)
+            {
+
+                sw.WriteLine((str + separator + random.Next(10,21).ToString() + separator + "true" + separator).PadRight(60) + "##");
+                str = sr.ReadLine();
+            }
+            sw.Close();
+            sr.Close();
+        }
+
+        public void AddPadding()
+        {
+            StreamReader sr = new StreamReader(BackUp);
+            StreamWriter sw = new StreamWriter(FileName, false);
+
+            string str = sr.ReadLine();
+
+            while (str != null)
+            {
+
+                sw.WriteLine(str.PadRight(42));
+                str = sr.ReadLine();
+            }
+            sw.Close();
+            sr.Close();
+        }
+
+        //prende una variabile dati e la transforma per renderla idonea ad essere scritta sul file
+        public string FileString(dati d)
+        {
+            return ((d.draw_date + separator + d.winning_numbers + separator + d.mega_ball + separator + d.multiplier + separator).PadRight(60) + "##");
+        }
+
+        public static dati FromString(string temp, string sep = ",")
+        {
+            //MessageBox.Show(temp);
+            dati p;
+            String[] fields = temp.Split(sep[0]);
+            //MessageBox.Show(fields[3]);
+            p.draw_date = fields[0];
+            p.winning_numbers = fields[1];
+            p.mega_ball = int.Parse(fields[2]);
+
+            
+
+            //controlla se il multiplier non é nullo
+            if (fields[3] != "")
+                p.multiplier = int.Parse(fields[3]);
+            else
+                p.multiplier = 1;
+
+            p.miovalore = int.Parse(fields[4]);
+            p.cancLogic = bool.Parse(fields[5]);
+
+            //dalla stringa deve ritornare la variabile dati p settata con i valori estratti
+            return p;
+        }
+
+
+        public void LoadFromFile(bool showw = false)
+        {
+            String line;
+            byte[] br;
+
+            var f = new FileStream(FileName, FileMode.Open, FileAccess.ReadWrite);
+            BinaryReader reader = new BinaryReader(f);
+            BinaryWriter writer = new BinaryWriter(f);
+
+
+
+            string[] words = new string[4];
+
+            listView.Items.Clear();
+
+            while (f.Position < f.Length - 2)
+            {
+
+                br = reader.ReadBytes(recordLength);
+                //converte in stringa
+                line = Encoding.ASCII.GetString(br, 0, br.Length);
+
+                //valore x debug per mostrare che linee legge
+                if (showw)
+                {
+                    MessageBox.Show(line);
+                }
+                //estraggo dalla stringa i valori e gli inserisco il d
+                d = FromString(line);
+
+                //aggiunge alla lista i dati di d
+                ListViewItem item = new ListViewItem(new[] { d.draw_date, d.winning_numbers, d.mega_ball.ToString(), d.multiplier.ToString() });
+                listView.Items.Add(item);
+
+            }
+
+            writer.Close();
+            reader.Close();
+            f.Close();
+
+        }
+
+        public void ModificaFile()
+        {
+            String line;
+            byte[] br;
+
+            var f = new FileStream(FileName, FileMode.Open, FileAccess.ReadWrite);
+            BinaryReader reader = new BinaryReader(f);
+            BinaryWriter writer = new BinaryWriter(f);
+
+
+
+            string[] words = new string[4];
+
+
+            while (f.Position < f.Length - 2)
+            {
+
+                br = reader.ReadBytes(recordLength);
+                //converte in stringa
+                line = Encoding.ASCII.GetString(br, 0, br.Length);
+
+                //estraggo dalla stringa i valori e gli inserisco il d
+                d = FromString(line);
+
+                if (d.draw_date == modify_draw_date_textbox.Text)
+                {
+                    switch (modify_target.Text)
+                    {
+                        case "WinNumbers":
+                            //MessageBox.Show("you are in modif winnumbers");
+                            d.winning_numbers = modify_new_value_textbox.Text;
+                            break;
+
+                        case "MegaBall":
+                            if (int.TryParse(modify_new_value_textbox.Text, out int number))
+                            {
+                                d.mega_ball = int.Parse(modify_new_value_textbox.Text);
+                            }
+                            else
+                            {
+                                MessageBox.Show("il valore inserito non è valido");
+                            }
+                            break;
+
+                        case "Multiplier":
+                            if (int.TryParse(modify_new_value_textbox.Text, out int number1))
+                            {
+                                d.multiplier = int.Parse(modify_new_value_textbox.Text);
+                            }
+                            else
+                            {
+                                MessageBox.Show("il valore inserito non è valido");
+                            }
+                            break;
+
+                        default:
+                            MessageBox.Show("valori inseriti non validi");
+                            break;
+                    }
+
+
+                    f.Seek(-recordLength, SeekOrigin.Current);
+                    writer.Write(Encoding.UTF8.GetBytes(FileString(d)));
+                }
+
+            }
+
+            writer.Close();
+            reader.Close();
+            f.Close();
+
+        }
+
+
+
+
+        //windows form functions
+        private void OpenButton_Click(object sender, EventArgs e)
+        {
+
+            ExecuteCommand(FileName);
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            LoadFromFile();
+        }
+
+        private void Modify_button_Click(object sender, EventArgs e)
+        {
+            ModificaFile();
+        }
+    }
+}
